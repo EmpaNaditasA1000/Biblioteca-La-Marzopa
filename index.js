469fac08dca81ce0f69aceeb63f1777479cf7758
EmpaNaditasA1000 <146375591+EmpaNaditasA1000@users.noreply.github.com>
2024-03-08 15:54:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("dbLaMarzopa.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 30001;
+const port = process.env.PORT || 30004;
 
 server.use(middlewares);
 server.use(router)
